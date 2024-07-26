f659622c528cf58cb84eb6d942d0bc2ed5c136e2
zft9xgy <zft9xgy@proton.me>
2024-07-27 00:34:58 +0200

    Fix: tags views

--- tags/models.py
+++ tags/models.py
@@ -13,13 +13,22 @@ class Tag(models.Model):
     @staticmethod
     def get_tags(number_of_tags : int = None) -> list:
         """
-        Devuelve una lista de tags donde cada tag tiene al menos un post. Exclude tags con 0 post.
+        Devuelve una lista de tags donde cada tag tiene al menos un post publicado. Exclude tags con 0 post.
         La lista de tags se ofrece ordenado por cantidad de post.
         Si se da un numero, este será el número máximo de tags que se devuelvan.
 
         """
-        tags = Tag.objects.annotate(note_count=Count('notes')).filter(Q(note_count__gt=0)).order_by('-note_count')
-        
+        #tags = Tag.objects.annotate(note_count=Count('notes')).filter(Q(note_count__gt=0)).order_by('-note_count')
+
+        tags = (
+            Tag.objects.annotate(
+                published_note_count=Count('notes', filter=Q(notes__status='published'))
+            )
+            .filter(published_note_count__gt=0)
+            .order_by('-published_note_count')
+        )
+
+
         if number_of_tags:
             return tags[:number_of_tags]
         
